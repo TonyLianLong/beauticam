c176881ded3687ecc1e5777277263f51c4c7d3c6
Tony Lian <1040424979@qq.com>
2018-07-14 22:48:45 +0800

    第一版

---
+++ README.md
@@ -0,0 +1,21 @@
+# beauticam - 给你的脸加上猫耳朵
+
+一个萌到炸的小创意，是我没事用业余时间做的，使用纯前端的方式给你的脸加上猫耳朵，虽然因为偷懒Vue.js用得不是很好，但至少能用了。猫耳朵是自己用AI画哒~ 喜欢的话就给个star咯。
+
+## 如何使用
+```shell
+# 在clone下来代码后
+yarn install
+yarn run serve
+```
+然后访问 http://localhost:8080 就可以啦
+
+### 编译 & Minify
+```shell
+yarn run build
+```
+
+### Lints （嗯…这里会跳出很多现有代码的问题，有待修复）
+```shell
+yarn run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app" class="demo-frame">
+    <div class="demo-container">
+      <video id="video" width="320" height="240" preload autoplay loop muted></video>
+      <canvas id="canvas" width="320" height="240"></canvas>
+    </div>
+    <img id="left" src='./assets/ears/left.png' style="display: none" />
+    <img id="right" src='./assets/ears/right.png' style="display: none" />
+  </div>
+</template>
+
+<script>
+import Tracking from 'tracking/build/tracking'
+import 'tracking/build/data/face'
+
+const OFFSET_X = -10;
+const OFFSET_Y = 0;
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  mounted: () => {
+    var ear_left = document.getElementById("left");
+    var ear_right = document.getElementById("right");
+    // TODO: wait until onload
+    var video = document.getElementById('video');
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
+      var tracker = new tracking.ObjectTracker('face');
+      tracker.setInitialScale(4);
+      tracker.setStepSize(2);
+      tracker.setEdgesDensity(0.1);
+
+      tracking.track('#video', tracker, { camera: true });
+
+      tracker.on('track', function(event) {
+        if (event.data.length == 0) {
+          return;
+        }
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        event.data.forEach(function(rect) {
+          /*context.strokeStyle = '#a64ceb';
+          context.strokeRect(rect.x, rect.y, rect.width, rect.height);*/
+          context.drawImage(ear_left, rect.x + OFFSET_X, rect.y + OFFSET_Y, 20, 30);
+          context.drawImage(ear_right, rect.x + rect.width + OFFSET_X, rect.y + OFFSET_Y, 20, 30);
+
+          /*context.font = '11px Helvetica';
+          context.fillStyle = "#fff";
+          context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+          context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);*/
+        });
+      });
+
+      var gui = new dat.GUI();
+      gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+      gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+      gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.demo-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  video, canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+</style>
